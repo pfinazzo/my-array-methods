3141d4fb56dcab51de5bd4f3881f928367577c35
Patrick Finazzo <pfinazzoproduction@gmail.com>
2018-09-05 22:17:36 -0700

    make everyOther array method

--- my-array-methods.js
+++ my-array-methods.js
@@ -1,5 +1,8 @@
-var a = [1,2,3];
-var b = [2,3,4];
+var a = [1,2,3,8];
+var b = [4,5,6,7];
+
+var c = [1,2,3,4,5,6];
+var d = [6,7,8,9]
 
 Array.prototype.mirrorConcat = function(newArr){
   for (var i = 0; i < newArr.length; i++){
@@ -33,6 +36,40 @@ Array.prototype.flattenAll = function(){
   return flatten();
 }
 
+
 // ["p","a","t",["r","i","c",["k",{name:"patrick"},"m",["n","o","p"]]]].flattenAll = ["p","a","t","r","i","c","k",{name:"patrick"},"m","n","o","p"]
 
+//[1,2,3].everyOther([4,5,6]) = [1,4,2,5,3,6];
 
+Array.prototype.everyOther = function(arr){
+  var largerArray;
+  var smallerArray;
+  var newArr = [];
+  var lengthDifference = arr.length - this.length;
+    if (lengthDifference < 0){
+      largerArray = this;
+      smallerArray = arr;
+      lengthDifference *= -1;
+    } else if (lengthDifference === 0){
+      largerArray = this;
+      smallerArray = arr;
+    } else {
+      largerArray = arr;
+      smallerArray = this;
+    }
+    largerArray.map((element, idx) => {
+     smallerArray.map((elem, index) => {
+        if (index === idx){
+          if (largerArray === arr){
+            newArr.push(elem, element)
+          } else {
+            newArr.push(element, elem);
+          }
+        }
+      })
+      if(idx > smallerArray.length - 1){
+        newArr.push(largerArray[idx]);
+      }
+    })
+  return newArr;
+}
